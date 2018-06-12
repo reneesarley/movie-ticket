2253618a0f6f690d994edd8421a5f5ef19ccd635
Nikki Boyd, Rita Bennett-Chew and Renee Sarley <renee@>
2018-06-12 12:01:44 -0700

    add Movie constructor

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,37 @@
-function Ticket(movieName, movieType, movieTime, discountType) {
+
+// userlogic
+
+$(function(){
+
+  $("#submitButton").click(function() {
+    var movieName = $('#titleSelection :selected').text();
+    var movieTime = $('#timeSelection :selected').text();
+    var movieDiscount = $('input:radio[name=discountInput]:checked').val();
+    var newTicket = new Ticket(movieName, movieTime, movieDiscount)
+    console.log(movieDiscount);
+    return newTicket;
+  })
+});
+
+function Movie(name, rating, times, type) {
+  this.movieName = movie
+  this.rating = rating
+  this.timesArray = times
+  this.type = type
+}
+
+// business logic
+function Ticket(movieName, movieTime, discountType) {
   this.movie = movieName
   this.mType = movieType
   this.mTime = movieTime
-  var showType = makeShowType(movieTime)
-  var cost = calcCost(movieType, discountType, showType)
+  var showType = this.makeShowType(movieTime)
+  var cost = this.calcCost(movieType, discountType, showType)
   this.cost = cost
 }
 
 
-function makeShowType(time) {
+Ticket.prototype.makeShowType = function(time) {
   var showType
   if(time <= 1600){
     showType = "matinee"
@@ -18,9 +41,8 @@ function makeShowType(time) {
   return showType
 }
 
-function calcCost(movieType, discountType, showType) {
+Ticket.prototype.calcCost = function(movieType, discountType, showType) {
   var cost = 16
-  console.log(movieType);
   if (movieType === "secondRun") {
     cost = cost - 2;
   } else {
